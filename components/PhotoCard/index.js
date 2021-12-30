62bcdd6875216157fe139e2b38a28b8598180106
Jean Paul <yjeanpaul@gmail.com>
2021-12-30 15:48:32 -0500

    Animaciones agregadas a la aplicacion

--- components/PhotoCard/index.js
+++ components/PhotoCard/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { ImgWrapper, Img, Button } from './styles';
 import { MdFavoriteBorder } from 'react-icons/md';
 
-const PhotoCard = ({ id, likes = 0, src }) => {
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
+
+const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   return (
     <article>
       <a href={`detail/${id}`}>
         <ImgWrapper>
-          <Img srC={src} />
+          <Img src={src} />
         </ImgWrapper>
       </a>
 
